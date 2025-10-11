ac8582e4f8a597c9ed283cf96a053d48896b947d
Huynh Nhan <imanhdanauvn123@gmail.com>
2025-10-11 17:19:23 +0700

    push code chưa còn đang dang dở để thiện xem mà làm

--- src/models/Table.java
+++ src/models/Table.java
@@ -7,7 +7,7 @@ import enums.TableState;
 public class Table {
     private final int size;
     private final List<Customer> customers;
-    private String[] FOOD_MENU = null; // menu của bàn
+    private List<String> FOOD_MENU = null; // menu của bàn
 
     // Constructor
     public Table(int size) {
@@ -23,7 +23,7 @@ public class Table {
 
     // add menu cho bàn
     public void addMenu() {
-        FOOD_MENU = SupplyManager.getManager().getMenu(); // Lấy menu của quán
+        FOOD_MENU = SupplyManager.getManager().randomMenuToday(10); // Lấy menu của quán
     }
 
     // Đưa khách hàng vào bàn nếu như vẫn còn chỗ ngồi
