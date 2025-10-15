dbc2e5fb000e4abf80ed269481b84c58a3eff7f8
Tai <taiaxolotlho735175@gmail.com>
2025-10-15 07:32:41 +0700

    Update ManagerHandler.java

--- src/contracts/ManagerHandler.java
+++ src/contracts/ManagerHandler.java
@@ -3,5 +3,8 @@ package contracts;
 public interface ManagerHandler {
     public void showGeneralInfo();
     public void createReport();
-    
+    public void add();
+    public void remove();
+    public void search();
 }
+
